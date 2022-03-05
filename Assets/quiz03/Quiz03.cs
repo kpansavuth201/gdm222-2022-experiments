0268c533f7ad5d2cdbd005a830aa6c733072ecbe
Pansavuth Khehasukcharoen <kpansavuth@gmail.com>
2022-03-05 17:04:22 +0700

    Quiz03 solution

--- Assets/quiz03/Quiz03.cs
+++ Assets/quiz03/Quiz03.cs
@@ -2,13 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//using System.Linq;
+
 public class Quiz03 : MonoBehaviour
 {
     private string ReverseCharacterForEachWords(string text) {
         // To do : Help me write this function
 
         // Hint : Separate words with String.Split https://docs.microsoft.com/en-us/dotnet/csharp/how-to/parse-strings-using-split
-        // string[] words = text.Split(' ');
+        string[] words = text.Split(' ');
+
+        string result = "";
+        for(int i = 0; i < words.Length; ++i) {
+            //Debug.Log(words[i]);
+            string reversedWord = ReverseCharInWord.Reverse( words[i] );
+            result = result + reversedWord;
+            if(i < words.Length - 1) {
+                result = result + " ";
+            }
+        }
+
+        // List<string> wordList = new List<string>(words);
+
+        // string result = "";
+        
+        // for(int i = 0; i < wordList.Count; ++i) {
+        //     wordList[i] = ReverseCharInWord.Reverse( wordList[i] );
+        //     Debug.Log(wordList[i]);
+
+        //     result = result + wordList[i];
+        //     if(i < wordList.Count - 1) {
+        //         result = result + " ";
+        //     }
+        // }
 
         // Hint : String can be converted to char array
         // char[] chars = someString.ToCharArray();
@@ -17,7 +43,7 @@ public class Quiz03 : MonoBehaviour
         // List<char> charList = new List<char>();
         // charList.AddRange(someString);
 
-        return text;
+        return result;
     }
 
     // You can write any other helper functions
